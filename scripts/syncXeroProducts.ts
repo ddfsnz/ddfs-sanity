d0a06647f4877e0a82177b35d0aee0e546fe2374
jcollicoat <jcollicoat@gmail.com>
2025-05-26 17:17:46 +0100

    update price

--- scripts/syncXeroProducts.ts
+++ scripts/syncXeroProducts.ts
@@ -26,7 +26,7 @@ const mockXeroData = {
         TaxType: 'INPUT2',
       },
       SalesDetails: {
-        UnitPrice: 72.0,
+        UnitPrice: 70.0,
         AccountCode: '200',
         TaxType: 'OUTPUT2',
       },
