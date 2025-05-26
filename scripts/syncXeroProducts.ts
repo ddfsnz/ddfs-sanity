ab6fdfa714ef3c4b7cfecef09f679ca1cd7c105a
jcollicoat <jcollicoat@gmail.com>
2025-05-26 13:48:11 +0100

    test

--- scripts/syncXeroProducts.ts
+++ scripts/syncXeroProducts.ts
@@ -104,16 +104,11 @@ const run = async () => {
   })
 
   mockXeroData.Items.map(async (product) => {
-    const document = await sanityClient.createIfNotExists(
-      {
-        _id: product.ItemID,
-        _type: 'product',
-        name: product.Name,
-      },
-      {
-        returnDocuments: true,
-      },
-    )
+    const document = await sanityClient.createIfNotExists({
+      _id: product.ItemID,
+      _type: 'product',
+      name: product.Name,
+    })
     // console.log(`Created product ${document.name} at ${document._createdAt}`)
     console.log(document)
   })
