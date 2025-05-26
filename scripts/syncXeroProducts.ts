eea707847761a7d75cb4bab8b82ce095078fa148
jcollicoat <jcollicoat@gmail.com>
2025-05-26 16:01:17 +0100

    handle patching published and drafts, creating new drafts

--- scripts/syncXeroProducts.ts
+++ scripts/syncXeroProducts.ts
@@ -62,30 +62,30 @@ const mockXeroData = {
       IsSold: true,
       IsPurchased: true,
     },
-    {
-      ItemID: '13b0ea2c-a913-42b1-a892-4a73343da9f9',
-      Code: '000002',
-      Description: 'Calvin Klein Eternity EDP For Him 100ml',
-      PurchaseDescription: 'Calvin Klein Eternity EDP For Him 100ml',
-      UpdatedDateUTC: '/Date(1724383341867+0000)/',
-      PurchaseDetails: {
-        UnitPrice: 32.75,
-        COGSAccountCode: '310',
-        TaxType: 'INPUT2',
-      },
-      SalesDetails: {
-        UnitPrice: 48.0,
-        AccountCode: '200',
-        TaxType: 'OUTPUT2',
-      },
-      Name: 'Calvin Klein Eternity EDP For Him 100ml [FAKE]',
-      IsTrackedAsInventory: true,
-      InventoryAssetAccountCode: '630',
-      TotalCostPool: 0.0,
-      QuantityOnHand: 0.0,
-      IsSold: true,
-      IsPurchased: true,
-    },
+    // {
+    //   ItemID: '13b0ea2c-a913-42b1-a892-4a73343da9f9',
+    //   Code: '000002',
+    //   Description: 'Calvin Klein Eternity EDP For Him 100ml',
+    //   PurchaseDescription: 'Calvin Klein Eternity EDP For Him 100ml',
+    //   UpdatedDateUTC: '/Date(1724383341867+0000)/',
+    //   PurchaseDetails: {
+    //     UnitPrice: 32.75,
+    //     COGSAccountCode: '310',
+    //     TaxType: 'INPUT2',
+    //   },
+    //   SalesDetails: {
+    //     UnitPrice: 48.0,
+    //     AccountCode: '200',
+    //     TaxType: 'OUTPUT2',
+    //   },
+    //   Name: 'Calvin Klein Eternity EDP For Him 100ml [FAKE]',
+    //   IsTrackedAsInventory: true,
+    //   InventoryAssetAccountCode: '630',
+    //   TotalCostPool: 0.0,
+    //   QuantityOnHand: 0.0,
+    //   IsSold: true,
+    //   IsPurchased: true,
+    // },
   ],
 }
 
@@ -105,22 +105,47 @@ const run = async () => {
 
   for (const product of mockXeroData.Items) {
     try {
-      const document = await sanityClient.createIfNotExists({
-        _id: `drafts.${product.ItemID}`,
-        _type: 'product',
-        name: product.Name,
-        price: product.SalesDetails.UnitPrice,
-      })
-      console.log(`✅ Processed ${document.name} at ${document._updatedAt}`)
+      const [publishedDoc, draftDoc] = await Promise.all([
+        sanityClient.fetch(`*[_type == "product" && _id == $id][0]`, {
+          id: product.ItemID,
+        }),
+        sanityClient.fetch(`*[_type == "product" && _id == $id][0]`, {
+          id: `drafts.${product.ItemID}`,
+        }),
+      ])
+
+      let document
+      if (publishedDoc) {
+        document = await sanityClient
+          .patch(product.ItemID)
+          .set({
+            name: product.Name,
+            price: product.SalesDetails.UnitPrice,
+          })
+          .commit()
+        console.log(`✅ Updated published product [${document.name}] at ${document._updatedAt}`)
+      } else if (draftDoc) {
+        document = await sanityClient
+          .patch(`drafts.${product.ItemID}`)
+          .set({name: product.Name, price: product.SalesDetails.UnitPrice})
+          .commit()
+        console.log(`✅ Updated draft product [${document.name}] at ${document._updatedAt}`)
+      } else {
+        document = await sanityClient.createIfNotExists({
+          _id: `drafts.${product.ItemID}`,
+          _type: 'product',
+          name: product.Name,
+          price: product.SalesDetails.UnitPrice,
+        })
+        console.log(`✅ Created draft product [${document.name}] at ${document._updatedAt}`)
+      }
     } catch (error) {
-      console.error(`❌ Failed to process ${product.Name} (ID: ${product.ItemID}):`, error)
+      console.error(
+        `❌ Failed to process product [${product.Name}] (ID: ${product.ItemID}):`,
+        error,
+      )
     }
   }
-
-  // Fetch Xero products
-  // Transform for Sanity schema
-  // createIfNotExist
-  // patch relevant product fields
 }
 
 run()
